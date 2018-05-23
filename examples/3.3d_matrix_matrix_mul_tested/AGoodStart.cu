f6026b898ba383c9c165c565e4fd8f2020a46f1d
Lu Li <lu.li@liu.se>
2018-05-23 23:26:04 +0200

    minor change to make it work on matrix mul on excess

--- examples/3.3d_matrix_matrix_mul_tested/AGoodStart.cu
+++ examples/3.3d_matrix_matrix_mul_tested/AGoodStart.cu
@@ -52,66 +52,6 @@ int main(int argc, char* argv[])
 	const size_t ha=problem_size, wa=problem_size, wb=problem_size;
 
 /*#ifdef BASE*/
-	{
-		float *a_h, *b_h, *c_h;
-		float *a_d, *b_d, *c_d;
-
-		const size_t size_a=ha*wa;
-		const size_t size_b=wa*wb;
-		/*const size_t size_c=ha*wb;*/
-
-		const size_t raw_size_a=ha*wa*sizeof(float);
-		const size_t raw_size_b=wa*wb*sizeof(float);
-		const size_t raw_size_c=ha*wb*sizeof(float);
-
-		a_h=(float *)malloc( raw_size_a );
-		b_h=(float *)malloc( raw_size_b );
-		c_h=(float *)malloc( raw_size_c);
-
-		cudaMalloc(&a_d, raw_size_a );
-		cudaMalloc(&b_d, raw_size_b );
-		cudaMalloc(&c_d, raw_size_c );
-
-		std::fill(a_h, a_h+size_a, 1.0f);
-		std::fill(b_h, b_h+size_b, 1.0f);
-
-		cudaMemcpy(a_d, a_h, raw_size_a, cudaMemcpyHostToDevice);
-		cudaMemcpy(b_d, b_h, raw_size_b, cudaMemcpyHostToDevice);
-
-		//dummy call to inialize
-		matrix_mul_cublas(a_d, b_d, c_d, ha, wa, wb);
-
-		using namespace meterpu;
-		meterpu::meter<meterpu::CPU_Time> my_meter;
-		/*meterpu::meter<meterpu::CUDA_Time> my_meter;*/
-
-		for(size_t i=0;i<100;++i){
-			std::cerr<<"Testing baseline, problem size: "<<problem_size<<", No."<<i<<std::endl;
-
-			my_meter.start();
-
-			matrix_mul_cublas(a_d, b_d, c_d, ha, wa, wb);
-
-			my_meter.stop();
-			my_meter.calc();
-			std::cout<<my_meter.get_value()<<","<<std::endl;
-		}
-
-		cudaMemcpy(c_h, c_d, raw_size_c, cudaMemcpyDeviceToHost);
-
-		/*std::for_each(c_h, c_h+size_c, [problem_size](float const i){assert(i==float(problem_size));}) ;*/
-
-
-		free(a_h);
-		free(b_h);
-		free(c_h);
-
-		cudaFree(a_d);
-		cudaFree(b_d);
-		cudaFree(c_d);
-
-
-	}
 
 /*#endif*/
 
@@ -120,20 +60,28 @@ int main(int argc, char* argv[])
 
 
 
-		tuneit::tuneit_settings<MATRIX_MUL_NUM_DIM, MATRIX_MUL_NUM_VARIANTS> st{2, std::vector<bool>(4,true), true, false, true, 40, {{1,1000}, {1,1000}, {1,1000}} };
+		tuneit::tuneit_settings<MATRIX_MUL_NUM_DIM, MATRIX_MUL_NUM_VARIANTS> st{2, std::vector<bool>(4,true), true, false, true, 5, {{1,10}, {1,10}, {1,10}} };
 
 		tuneit::tuneit< MATRIX_MUL_NUM_VARIANTS, 8, matrix_mul_tunable<float, size_t, size_t, size_t>, float, size_t, size_t, size_t> mytuner(st);
 
+		std::cout<<"before train"<<std::endl;
+
 		mytuner.train();
+
+		std::cout<<"after train"<<std::endl;
 
 		vectorpu::vector<float> a(wa*ha,1), b(wa*wb,1), c(ha*wb,0);
 
+		std::cout<<"before run"<<std::endl;
+
 		mytuner.run(mytuner.predict(ha,wa,wb), a, b, c, ha, wa, wb);
+
+		std::cout<<"after run"<<std::endl;
 
 		using namespace meterpu;
 		meterpu::meter<meterpu::CPU_Time> my_meter;
 
-		for(size_t i=0;i<100;++i){
+		for(size_t i=0;i<3;++i){
 			std::cerr<<"Testing opt, problem size: "<<problem_size<<", No."<<i<<std::endl;
 			my_meter.start();
 
